b34000e6bdb3e0467d2d2603bc19bae064637291
Princess Sampson <ps@kingonomics.com>
2015-09-29 18:42:56 -0400

    Initial commit.

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e6/b057edc2e06600151759d49d8b0d3a19
@@ -0,0 +1,6 @@
+
+public class HelloWorldEclipse {
+	public static void main(String [] args){
+		
+	}
+}
---
+++ hw/day1hw/EvenNumbers.java
@@ -0,0 +1,12 @@
+class EvenNumbers {
+  public static void main(String [] args) {
+    int x;
+
+    for(x = 0; x<101; x++)
+
+      if(( x % 2) == 0){
+        System.out.println("Counting to " + x + ".");
+      }
+
+  }
+}
