ae98423a7cfc039b61670e6787b34a45550412ec
Princess Sampson <ps@kingonomics.com>
2015-10-06 16:32:26 -0400

    moved wheres waldo to method

--- day6/pairprogramming4/Methods.java
+++ day6/pairprogramming4/Methods.java
@@ -1,9 +1,21 @@
 
 public class Methods {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	void wheresWaldo(String[]  arr) {
+//		Iterate until less or equal to the length of the array.
+		for (int i = 0; i < arr.length; i++) {
+			
+//			If the value of the current index, lowercased, equals "waldo", print a message and the index. Else print out a message.
+			if(arr[i].toLowerCase().equals("waldo")) {
+				
+				System.out.println("Waldo is at index no. " + i + ".");
+			
+			} else {
+				
+				System.out.println("Not Waldo!");
+			}
+			
+		}
 	}
-
+	
 }
