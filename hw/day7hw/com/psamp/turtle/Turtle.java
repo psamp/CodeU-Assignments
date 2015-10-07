62c96862a306a2d181df40b291c06f264553a4fa
Princess Sampson <ps@kingonomics.com>
2015-10-07 05:51:38 -0400

    finished turtle assignment

---
+++ hw/day7hw/com/psamp/turtle/Turtle.java
@@ -0,0 +1,34 @@
+package com.psamp.turtle;
+
+class Turtle {
+	int age = 0;
+	
+//	Take in an int, return an int.
+	int ageUp(int years) {
+		this.age += years;
+		
+		return this.age;
+	}
+	
+//	Take in an int, return nothing.
+	void eatLeaves(int leaves) {
+		System.out.println("Ate " + leaves + " leaves." );
+	}
+	
+//	Take in a double, return double.
+	double walkAround(double steps) {
+		return steps;
+	}
+	
+//	Take in nothing, return a string.
+	String snapSnap() {
+		return "*wendigo noises*";
+	}
+	
+//	Take in nothing, return nothing.
+	void wagTail() {
+		System.out.println("*wags tail*");
+	}
+	
+
+}
