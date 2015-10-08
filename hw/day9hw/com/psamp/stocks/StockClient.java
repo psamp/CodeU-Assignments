99445cf2284929e6d4399f6c3416b9437158e9e1
Princess Sampson <ps@kingonomics.com>
2015-10-08 01:23:27 -0400

    attempted to sort per

--- hw/day9hw/com/psamp/stocks/StockClient.java
+++ hw/day9hw/com/psamp/stocks/StockClient.java
@@ -6,7 +6,7 @@ class StockClient {
 //	Start program.
 	public static void main(String[] args) {
 		
-//		New array of Stocks.
+//		New array of Stock objects.
 		Stock stocks[] = {
 				new Stock("ABC", 20.01, 1.30),
 				new Stock("DEF", 30.11, .99),
@@ -15,20 +15,21 @@ class StockClient {
 				new Stock ("MNO", 23.55, 4.21)
 		};
 		
-//		New array of StockPERatio.
+//		New array of StockPERatio objects.
 		StockPERatio per[] = new StockPERatio[stocks.length];
 		
-//		Give every index in per to the value of a StockPERatio object, and call its constructor and pass in the stockName and peRatio.
+//		Give every index in per the value of a StockPERatio object, and call its constructor and pass in the stockName and peRatio.
 		for (int i = 0; i < per.length; i++) {	
 			per[i] = new StockPERatio(stocks[i].stockName, stocks[i].peRatio);
 		}
 		
 /*		
-* 		For every index of per, grab the name and the peRatio. To obtain two decimal places, 
+* 		For every index of per, grab the name and the peRatio and print it. To obtain two decimal places, 
 * 		pass peRatio multiplied times one hundred into Math.floor() (finds the smallest int a decimal can go down to) 
 * 		and divide the product by one hundred to get the correct number.
 */		
 		for (int i = 0; i < per.length; i++) {
+			
 			System.out.println(per[i].name + " | " + Math.floor(per[i].peRatio * 100) / 100);
 		}
 		
